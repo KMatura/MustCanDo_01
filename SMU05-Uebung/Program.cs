5b89a6f22e76c6e41ece6687e91ce9d8f526146e
KMatura <matura.k04@htlwienwest.at>
2021-01-04 12:57:21 +0100

    final

--- SMU05-Uebung/Program.cs
+++ SMU05-Uebung/Program.cs
@@ -15,6 +15,21 @@ namespace UebungMustCanDo01
         static string obstSchüssel = "Pflaumen;Apfel;Limonen;Brombeere;Brombeere;Apfel;Mango;Limone;Erdbeer;Apfel;Birnen;Wassermelone;Feigen;Apfel;Trauben;Birnen;Zitronen;Apfel;Birnen;Feigen;;;";
 
         // hier implementieren
+        public static int summeZahlen(string line)
+        {
+            int summe = 0;
+            string[] zeileSplitted = line.Split(' ');
+            foreach (var item in zeileSplitted)
+            {
+                int itemInt = Convert.ToInt32(item);
+                if (itemInt > 50)
+                {
+                    summe += itemInt;
+                    
+                }
+            }
+            return summe;
+        }
         // diese Funktion soll die Datei uebung01 zeilenweise einlesen
         // in jeder Zeile stehen Zahlen durch ein Leerzeichen getrennt
         // alle Zahlen aller Zeilen die größer als 50 sind, sollen aufsummiert werden
@@ -37,9 +52,17 @@ namespace UebungMustCanDo01
         // Diese Funktion soll eine neue Liste an Obst zurückgeben.
         // Die Liste die übergeben wird enthält "Birne", die zurückgegebene Liste soll keine Birnen mehr enthalten.
         // Oder anders formuliert: Erstelle eine neue Liste, die alles bis auf "Birne" von der Originalliste enthält.
-        private static List<string> ListeOhneBirnen(List<string> obstL)
+        static List<string> OhneBirnen = new List<string>();
+        public static List<string> ListeOhneBirnen(List<string> obstListe)
         {
-            throw new NotImplementedException();
+            foreach (var item in obstListe)
+            {
+                if (item != "Birne")
+                {
+                    OhneBirnen.Add(item);
+                }
+            }
+            return obstListe;
         }
 
 
@@ -55,8 +78,29 @@ namespace UebungMustCanDo01
 
 
         // hier ObstKlein implementieren
+        public static void ObstKlein(string obst)
+        {
+            if (obst == "Apfel" | obst == "Birne" | obst == "Brombeere")
+            {
+                Console.WriteLine($"{obst}mus");
+            }
+            else
+            {
+                Console.WriteLine($"{obst}würfel");
+            }
+
+        }
+        
         // hier MacheObstSalat implementieren
 
+        public static void MacheObstSalat(string obstSchüssel)
+        {
+            string[] Früchte = obstSchüssel.Split(';');
+            foreach (var item in Früchte)
+            {
+                ObstKlein(item);
+            }
+        }
 
 
         static void Main(string[] args)
